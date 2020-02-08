ddb52ae48c17b19d412bd76505e23fe044dc4d00
rahularya <rahularya@berkeley.edu>
2020-02-08 01:57:22 -0800

    more aggressive shortening

--- src/piazza_integration.py
+++ src/piazza_integration.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 from collections import namedtuple
 from html import unescape
@@ -5,25 +6,30 @@ from html import unescape
 from auth import query
 from integration import Integration
 
-REGEX = "@([0-9]+)(_f([0-9]+))?"
+COURSE = query("piazza/course_id", staff=True)
+
+SHORT_REGEX = r"@(?P<cid>[0-9]+)(_f(?P<fid>[0-9]+))?"
+LONG_REGEX = r"https://piazza.com/class/{}\?cid=(?P<cid>[0-9]+)(_f(?P<fid>[0-9]+))?".format(COURSE)
 
 Post = namedtuple("Post", ["subject", "content", "url", "full_cid"])
 
 
 class PiazzaIntegration(Integration):
     def process(self):
-        self.posts = []
-        course = None
-        for match in re.finditer(REGEX, self.message):
-            full_cid = match.group(1) + (match.group(2) or "")
-            cid = int(match.group(1))
+        self.posts = {}
+        for match in itertools.chain(
+                re.finditer(SHORT_REGEX, self.message),
+                re.finditer(LONG_REGEX, self.message),
+        ):
+            cid = int(match.group("cid"))
+            fid_str = match.group("fid")
+            full_cid = match.group("cid") + ("_f{}".format(fid_str) if fid_str else "")
             post = query("piazza/get_post", staff=True, cid=cid)
-            course = course or query("piazza/course_id", staff=True)
             subject = post["history"][0]["subject"]
             content = post["history"][0]["content"]
 
-            if match.group(3):
-                fid = int(match.group(3))  # 1 indexed
+            if fid_str:
+                fid = int(fid_str)  # 1 indexed
                 curr_id = 0
                 for child in post["children"]:
                     if child["type"] != "followup":
@@ -36,15 +42,17 @@ class PiazzaIntegration(Integration):
                 content = child["subject"]
 
             content = unescape(re.sub("<[^<]+?>", "", content))
-            url = "https://piazza.com/class/{}?cid={}".format(course, full_cid)
+            url = "https://piazza.com/class/{}?cid={}".format(COURSE, full_cid)
 
-            self.posts.append(Post(subject, content, url, full_cid))
+            self.posts[Post(subject, content, url, full_cid)] = None
 
     @property
     def text(self):
         out = self.message
         for post in self.posts:
-            out = out.replace("@{}".format(post.full_cid), "<{}|@{}>".format(post.url, post.full_cid))
+            link = "<{}|@{}>".format(post.url, post.full_cid)
+            out = out.replace("<{}>".format(post.url), link)
+            out = re.sub(r"(?<!\|)(@{})".format(post.full_cid), link, out)
         return out
 
     @property
